a4e33b9110e0dcab5cb1a3279165fe5fb176064a
OciMM <bekasovmaks20@gmail.com>
2024-05-28 17:16:41 +0300

    fix api

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://127.0.0.1:8000/api/"
-export const API_URL_FOREIGN_FRONTEND = "http://31.129.96.225/"
-export const API_URL_FOREIGN_API = "http://31.129.96.225/api/"
+export const API_URL = "http://62.113.96.70/api/";
+export const API_URL_FOREIGN_FRONTEND = "http://31.129.96.225/";
+export const API_URL_FOREIGN_API = "http://31.129.96.225/api/";
